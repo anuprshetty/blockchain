35184c43dea33f3d73a6ade37e6c69ac597642d6
anuprshetty <anuprshetty@gmail.com>
2018-06-23 16:23:55 +0530

    Block class setup

---
+++ app/block.js
@@ -0,0 +1,16 @@
+import hexToBinary from "hex-to-binary";
+import { GENESIS_DATA, MINE_RATE } from "./config.js";
+import cryptoHash from "./crypto-hash.js";
+
+class Block {
+  constructor({ timestamp, data, prevHash, difficulty, nonce, hash }) {
+    this.timestamp = timestamp;
+    this.data = data;
+    this.prevHash = prevHash;
+    this.difficulty = difficulty;
+    this.nonce = nonce;
+    this.hash = hash;
+  }
+}
+
+export default Block;
