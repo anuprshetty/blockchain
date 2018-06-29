c228b9fd5b96dff49a30586519d89ff468463cc0
anuprshetty <anuprshetty@gmail.com>
2018-06-29 13:26:57 +0530

    logic for mining a block

--- app/block.js
+++ app/block.js
@@ -16,6 +16,35 @@ class Block {
     return new this(GENESIS_DATA);
   }
 
+  static mineBlock({ data, prevBlock }) {
+    const prevHash = prevBlock.hash;
+
+    let timestamp, difficulty, hash;
+    let nonce = 0;
+    do {
+      timestamp = Date.now();
+      nonce++;
+
+      difficulty = Block.adjustDifficulty({
+        timestamp,
+        originalBlock: prevBlock,
+      });
+
+      hash = cryptoHash(timestamp, data, prevHash, difficulty, nonce);
+    } while (
+      hexToBinary(hash).substring(0, difficulty) !== "0".repeat(difficulty)
+    );
+
+    return new this({
+      timestamp,
+      prevHash,
+      data,
+      difficulty,
+      nonce,
+      hash,
+    });
+  }
+
   static adjustDifficulty({ timestamp, originalBlock }) {
     const prevDifficulty = originalBlock.difficulty;
     if (prevDifficulty < 1) {
