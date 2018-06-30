060273afaf5f0b9d982e4cd1d0c6e206b7c7c2d2
anuprshetty <anuprshetty@gmail.com>
2018-06-30 20:30:02 +0530

    logic to add a block to the blockchain

--- app/blockchain.js
+++ app/blockchain.js
@@ -5,6 +5,17 @@ class Blockchain {
   constructor() {
     this.chain = [Block.genesis()];
   }
+
+  addBlock({ data }) {
+    const prevBlock = this.chain[this.chain.length - 1];
+
+    const newBlock = Block.mineBlock({
+      data,
+      prevBlock,
+    });
+
+    this.chain.push(newBlock);
+  }
 }
 
 export default Blockchain;
