df28cb71030b9abd3b2d0b9c8254340ac8614bdd
anuprshetty <anuprshetty@gmail.com>
2018-07-03 20:47:19 +0530

    check whether its a valid chain or not

--- app/blockchain.js
+++ app/blockchain.js
@@ -16,6 +16,39 @@ class Blockchain {
 
     this.chain.push(newBlock);
   }
+
+  static isValidChain(chain) {
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+      return false;
+    }
+
+    for (let i = 1; i < chain.length; i++) {
+      const { timestamp, data, prevHash, difficulty, nonce, hash } = chain[i];
+
+      const realPrevHash = chain[i - 1].hash;
+      if (prevHash !== realPrevHash) {
+        return false;
+      }
+
+      const validatedHash = cryptoHash(
+        timestamp,
+        data,
+        prevHash,
+        difficulty,
+        nonce
+      );
+      if (hash !== validatedHash) {
+        return false;
+      }
+
+      const prevDifficulty = chain[i - 1].difficulty;
+      if (Math.abs(difficulty - prevDifficulty) > 1) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
 
 export default Blockchain;
