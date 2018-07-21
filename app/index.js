65830ac8adf855e465a7267d7ca90a1ed8cac7db
anuprshetty <anuprshetty@gmail.com>
2018-07-21 22:46:02 +0530

    mine block api

--- app/index.js
+++ app/index.js
@@ -17,6 +17,15 @@ app.get("/api/chain/", (req, res) => {
   res.json(pubsub.blockchain.chain);
 });
 
+app.post("/api/mine_block/", (req, res) => {
+  const { data } = req.body;
+
+  pubsub.blockchain.addBlock({ data });
+  pubsub.broadcastChain();
+
+  res.json(pubsub.blockchain.chain);
+});
+
 app.listen(process.env.APP_PORT, () => {
   console.log(`Listening on PORT:${process.env.APP_PORT}\n`);
   syncChain();
