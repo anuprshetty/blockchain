ad1c01250aea6316940eb9c62659783c78713f74
anuprshetty <anuprshetty@gmail.com>
2018-07-24 08:13:50 +0530

    synchronizing blockchain with main node on peer node start

--- app/index.js
+++ app/index.js
@@ -26,6 +26,27 @@ app.post("/api/mine_block/", (req, res) => {
   res.json(pubsub.blockchain.chain);
 });
 
+const MAIN_NODE_URL = `http://localhost:3000/api/chain/`;
+
+const syncChain = () => {
+  request({ url: `${MAIN_NODE_URL}` }, (error, reposnse, body) => {
+    if (!error && reposnse.statusCode === 200) {
+      const chain = JSON.parse(body);
+      pubsub.blockchain.replaceChain(chain);
+
+      console.log(
+        `\nInitial blockchain synchronization is successful!\nSynced chain: ${JSON.stringify(
+          pubsub.blockchain.chain,
+          null,
+          2
+        )}\n`
+      );
+    } else {
+      console.error("Initial blockchain synchronization is failed.\n");
+    }
+  });
+};
+
 app.listen(process.env.APP_PORT, () => {
   console.log(`Listening on PORT:${process.env.APP_PORT}\n`);
   syncChain();
