e52646daed3d62b786e49f279b675628489a3928
anuprshetty <anuprshetty@gmail.com>
2018-08-04 23:44:55 +0530

    hex 0x prefix bug fix

--- app/config.js
+++ app/config.js
@@ -3,10 +3,10 @@ const INITIAL_DIFFICULTY = 10;
 const GENESIS_DATA = {
   timestamp: 1,
   data: ["block_genesis"],
-  prevHash: "0x000",
+  prevHash: "000",
   difficulty: INITIAL_DIFFICULTY,
   nonce: 0,
-  hash: "0x123",
+  hash: "123",
 };
 
 export { GENESIS_DATA, MINE_RATE };
--- app/crypto-hash.js
+++ app/crypto-hash.js
@@ -3,7 +3,7 @@ import { createHash } from "crypto";
 const cryptoHash = (...inputs) => {
   const hash = createHash("sha256");
   hash.update(inputs.sort().join(""));
-  return "0x" + hash.digest("hex");
+  return hash.digest("hex");
 };
 
 export default cryptoHash;
